f9fd70ef424516b2543c657353a1eea1f0ddd911
Bartosz Olszewski <bartosz.olszewski@aiclearing.com>
2024-10-21 18:23:55 +0200

    added i3 de utils

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,8 @@
+killall polybar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload toph --config=$HOME/.config/polybar/config.ini &
+  done
+else
+  polybar --reload toph &
+fi
