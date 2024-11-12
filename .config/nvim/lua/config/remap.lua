194dd738e1ce51f4e85b70fc2bdd3f41f35bbc77
Bartosz Olszewski <bartosz.olszewski@aiclearing.com>
2024-11-12 14:02:11 +0100

    added scripts to nvim

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -31,11 +31,38 @@ vim.keymap.set(
     "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
 )
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
+vim.keymap.set("n", "<C-t>", function()
+    local cwd = vim.fn.expand("%:p:h")
+    vim.cmd(string.format("silent !tmux split-window -h -c %s", cwd))
+end)
 
+
+vim.keymap.set("n", "<leader>h", function()
+    local registers = {}
+    for _, reg in ipairs({'"', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', ':', '.', '/', '%', '#', '_'}) do
+        local content = vim.fn.getreg(reg)
+        if content ~= "" then
+            table.insert(registers, string.format('%s: %s', reg, content:gsub("\n", "\\n")))
+        end
+    end
+
+    local fzf = require("fzf-lua")
+    fzf.fzf_exec(registers, {
+        prompt = 'Select register: ',
+        actions = {
+            -- Callback to paste selected register
+            ['default'] = function(selected)
+                if selected[1] then
+                    -- Extract the register name (first character before ':')
+                    local reg = selected[1]:sub(1, 1)
+                    -- Insert the selected register's content into the buffer
+                    vim.api.nvim_put({vim.fn.getreg(reg)}, 'c', true, true)
+                end
+            end,
+        },
+    })
+end)
