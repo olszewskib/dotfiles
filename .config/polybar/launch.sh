a143aead6669c199ae5dfffe4b4d6e6c9e75b684
Bartosz Olszewski <bartosz.olszewski@aiclearing.com>
2025-05-16 13:43:07 +0200

    updated the name of the bar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,8 +1,8 @@
 killall polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph --config=$HOME/.config/polybar/config.ini &
+    MONITOR=$m polybar --reload main --config=$HOME/.config/polybar/config.ini &
   done
 else
-  polybar --reload toph &
+  polybar --reload main &
 fi
